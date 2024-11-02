9a6115314293f8cf7b1cadb59761cd361c28af18
Cyclosaryn <mcj.verbroekken@gmail.com>
2024-11-03 00:12:39 +0100

    Add health check endpoint to URLs

--- cycloforge/portfolio/urls.py
+++ cycloforge/portfolio/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
+    path('health/', views.health_check, name='health_check'),
 ]
