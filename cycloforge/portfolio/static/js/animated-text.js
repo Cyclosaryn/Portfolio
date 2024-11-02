d8a7766363dfcdddaad4b1f26185a377e78471d6
Cyclosaryn <mcj.verbroekken@gmail.com>
2024-11-02 14:35:35 +0100

    Add initial project files and configurations

---
+++ cycloforge/portfolio/static/js/animated-text.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let texts = ["Back-end developer.", "Front-end developer.", "Fullstack developer.", "Python developer.", "JavaScript developer.", "Django developer.", "NodeJS developer.", "Electron developer."];
+    
+    // Function to shuffle the array
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    shuffle(texts); // Shuffle the texts array
+
+    let index = 0;
+    let charIndex = 0;
+    const typingSpeed = 50;
+    const erasingSpeed = 10;
+    const delayBetweenTexts = 2000;
+    const animatedTextElement = document.getElementById('animated-text');
+
+    function type() {
+        if (charIndex < texts[index].length) {
+            animatedTextElement.textContent += texts[index].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingSpeed);
+        } else {
+            setTimeout(erase, delayBetweenTexts);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            animatedTextElement.textContent = texts[index].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingSpeed);
+        } else {
+            index = (index + 1) % texts.length;
+            setTimeout(type, typingSpeed);
+        }
+    }
+
+    type();
+});
---
+++ cycloforge/portfolio/views.py
@@ -0,0 +1,5 @@
+# portfolio/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
