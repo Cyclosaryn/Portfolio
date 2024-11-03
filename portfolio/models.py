1963d5ea9990eb54d40ec465c079c3f3b7626408
Cyclosaryn <mcj.verbroekken@gmail.com>
2024-11-03 01:46:04 +0100

    Add initial files for portfolio and CycloForge apps; remove unused files

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 from tinymce.models import HTMLField
 
-# Create your models here.
 class Biography(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,16 @@
 # portfolio/views.py
 from django.shortcuts import render
 from .models import Biography
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 def home(request):
-    biography = Biography.objects.filter(name='Homepage').latest('created_at')
+    try:
+        biography = Biography.objects.filter(name='Homepage').latest('created_at')
+    except Biography.DoesNotExist:
+        biography = None
     context = {
         'biography': biography
     }
