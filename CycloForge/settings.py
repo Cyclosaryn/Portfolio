14601be50716ed0100104df32de2d413ec05a254
Cyclosaryn <mcj.verbroekken@gmail.com>
2025-01-02 00:43:55 +0100

    Refactor static file references in settings and templates for consistency

--- CycloForge/settings.py
+++ CycloForge/settings.py
@@ -142,13 +142,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'portfolio/static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
